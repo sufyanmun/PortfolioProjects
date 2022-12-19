c8934d1d1f066b17a321dbdbccd4a1bb9f4925f5
Sufyan Mun <60675257+sufyanmun@users.noreply.github.com>
2022-12-19 16:00:52 -0500

    Update Hotel Bookings Project.sql

--- Hotel Booking Project/Hotel Bookings Project.sql
+++ Hotel Booking Project/Hotel Bookings Project.sql
@@ -1,9 +1,12 @@
+-- Calculates the Revenue by adding the stays and multiplying it by the average daily rate (ADR)
+
+SELECT (stays_in_week_nights+stays_in_weekend_nights)*adr AS Revenue FROM travel_booking_data
 
 -- Revenue per year for each hotel type sorted by year
 SELECT arrival_date_year AS Year, hotel AS Hotel_Type, SUM((stays_in_week_nights+stays_in_weekend_nights)*adr) AS Revenue
-FROM hotel_d WHERE hotel IS NOT NULL GROUP BY arrival_date_year, hotel ORDER BY Year;
+FROM travel_booking_data WHERE hotel IS NOT NULL GROUP BY arrival_date_year, hotel ORDER BY Year;
 
 
 -- Shows the busiest month ranked by number of bookings
-SELECT DISTINCT COUNT(*) AS Number_of_bookings, arrival_date_month FROM travel_booking_data.hotel_bookings 
+SELECT DISTINCT COUNT(*) AS Number_of_bookings, arrival_date_month FROM travel_booking_data 
 WHERE is_canceled ="0" GROUP BY arrival_date_month ORDER BY Number_of_bookings DESC;
